2d876cdf308d4434168327c0ce4b6db253d33bd5
Md Abdullah Al Masum <masum06aiub@gmail.com>
2023-08-21 07:48:37 +0600

    Next blog project

---
+++ app/blog/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../components/Navbar';
+
+export default function page() {
+  return (
+    <>
+    <Navbar/>
+    <div>This is blog page.</div>
+  
+  </>
+  )
+}
---
+++ app/components/Navbar.js
@@ -0,0 +1,65 @@
+// components/Navbar.js
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link href="/">
+          <a className="navbar-brand">My Website</a>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link href="/">
+                <a className="nav-link">Home</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about">
+                <a className="nav-link">About</a>
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Services
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link href="/services/service1">
+                    <a className="dropdown-item">Service 1</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/service2">
+                    <a className="dropdown-item">Service 2</a>
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
